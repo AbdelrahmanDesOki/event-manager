65bfa53ba3aeeabc569c7167a7e1775c1b7b601a
Desokii <desoki.abdelrahman@gmail.com>
2022-10-14 14:21:49 +0200

    Last edition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './App.css';
+
+import React, { useEffect } from 'react';
+import 'src/App.css';
 import Navbar from './components/Navbar/indexNav';
-import{BrowserRouter as Router} from 'react-router-dom'
+import{BrowserRouter as Router} from 'react-router-dom';
+import axios from 'axios';
 function App() {
+
+  const  fetchapi = async() => {
+    try{
+ const res = await axios.get('http://localhost:3000/');
+ console.log(res.data);
+    }catch(error){
+     console.log(error.message);
+    }
+  };
+  useEffect(
+    () => {
+      fetchapi();
+    }, [] 
+  );
+
+
   return (
-    <Router>
+    <Router >
       <Navbar/>
     </Router>
   );
 }
+
+
 
 export default App;
