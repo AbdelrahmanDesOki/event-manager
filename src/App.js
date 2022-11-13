570acce81e5262b2dbf5f225aea6c17a134ec421
babalex16 <sasha.babii@mail.ru>
2022-11-13 12:09:52 +0100

    Revert "Merge branch 'BEconnFE' of https://github.com/AbdelrahmanDesOki/event-manager into BEconnFE"
    This reverts commit f6e282f2b3fa56fb24addf42769ac2765a43ad4e, reversing
    changes made to df18415b3c9af4a850e8b2aa28b80a1ff516ccb6.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
-import React, { useEffect } from 'react';
-import 'src/App.css';
-import Navbar from './components/Navbar/indexNav';
-import{BrowserRouter as Router} from 'react-router-dom';
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Components/Login/Auth"
+import Navbar from "./Components/Navbar/indexNav";
+import Carousel from "./Components/Carousel/Carousel"
+import Payment from "./Components/paymentPage/paymentUi"
+import HomePage from "./Components/HomePage/HomePage";
 import axios from 'axios';
+import News from "./Components/HomePage/News";
+import MainPage from "./Components/MainPage";
+
 function App() {
 
   const  fetchapi = async() => {
