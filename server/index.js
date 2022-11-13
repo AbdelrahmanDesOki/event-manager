be775ff6d4642cc947531293090d66c5c4f8702b
Desokii <desoki.abdelrahman@gmail.com>
2022-11-13 12:31:14 +0100

    starting the backend of the payment page

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+
+
+app.request(express.json())
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storedItems = new Map([
+    [1, {priceInCents: 10000, name:"bag"}]
+    [2, {priceInCents: 20000, name:"clothes"}]
+])
 
 app.get('/api', (req, res) => {
     res.send("Hello from server");
