c4626df7731657547a7f7ae419e0eddb581d4108
babalex16 <sasha.babii@mail.ru>
2022-11-13 16:04:27 +0100

    removed payment due to errors

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./Components/Login/Auth"
 import Navbar from "./Components/Navbar/indexNav";
-import Carousel from "./Components/Carousel/Carousel"
-import Payment from "./Components/paymentPage/paymentUi"
-import HomePage from "./Components/HomePage/HomePage";
 import axios from 'axios';
-import News from "./Components/HomePage/News";
-
 import MainPage from "./Components/MainPage";
-import axios from 'axios';
 
 function App() {
   const fetchapi = async () => {
@@ -34,7 +28,6 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage/>} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/pay" element={<Payment />} />
         </Routes>
         
       </div>
