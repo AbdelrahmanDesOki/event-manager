2b915bcebcbbc4361d7c912540562783624bf9e0
Desokii <desoki.abdelrahman@gmail.com>
2022-12-08 21:25:05 +0100

    fixing some links and styles

--- src/Components/HomePage/EventList.js
+++ src/Components/HomePage/EventList.js
@@ -6,27 +6,27 @@ const EventList = () => {
         <>
             <h2 style={{marginBottom: "40px"}}>EVENTS</h2>
             <p>
-                <Link to="#" class="bold" >*Full Name of the event Ea adipisicing nulla pariatur voluptate sit ex eu ut.*</Link>  
+                <Link to="/elem" class="bold" >Santa Claus for next Month Soon!</Link>  
                 <br/>
-                <span class="muted font-size-sm"> *Date and location of the event*</span>
+                <span class="muted font-size-sm"> *04-Dec-2022*</span>
                 <hr style={{color: "white"}}/>
             </p>
             <p>
-                <Link to="#" class="bold" >*Full Name of the event Ea adipisicing nulla pariatur voluptate sit ex eu ut.*</Link>  
+                <Link to="#" class="bold" >Tennis Event in München</Link>  
                 <br/>
-                <span class="muted font-size-sm"> *Date and location of the event*</span>
+                <span class="muted font-size-sm"> *23-01-2023*</span>
                 <hr style={{color: "white"}}/>
             </p>
             <p>
-                <Link to="#" class="bold" >*Full Name of the event Ea adipisicing nulla pariatur voluptate sit ex eu ut.*</Link>  
+                <Link to="#" class="bold" >Boxing Champoinship</Link>  
                 <br/>
-                <span class="muted font-size-sm"> *Date and location of the event*</span>
+                <span class="muted font-size-sm"> 02-12-2023</span>
                 <hr style={{color: "white"}}/>
             </p>
             <p>
-                <Link to="#" class="bold" >*Full Name of the event Ea adipisicing nulla pariatur voluptate sit ex eu ut.*</Link>  
+                <Link to="#" class="bold" >Swimming open Day in Spring</Link>  
                 <br/>
-                <span class="muted font-size-sm"> *Date and location of the event*</span>
+                <span class="muted font-size-sm"> 04-03-2023</span>
                 <hr style={{color: "white"}}/>
             </p>
         </>
